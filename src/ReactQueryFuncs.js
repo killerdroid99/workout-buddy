b6f8f1f91efe8c30026daca8ffe3038e02df7be5
killerdroid099 <ajempire09@gmail.com>
2022-07-02 23:47:26 +0530

    Added functions for CRUD operations

--- src/ReactQueryFuncs.js
+++ src/ReactQueryFuncs.js
@@ -1,6 +1,26 @@
 import axios from "axios";
 
 export async function fetchWorkouts() {
-	const res = await axios.get(import.meta.env.VITE_BACKEND_URL);
-	return res.data;
+	try {
+		const res = await axios.get(import.meta.env.VITE_BACKEND_URL);
+		return res.data;
+	} catch (error) {
+		return error;
+	}
+}
+
+export async function addWorkout(workout) {
+	try {
+		const res = await axios.post(import.meta.env.VITE_BACKEND_URL, workout);
+	} catch (error) {
+		return error;
+	}
+}
+
+export async function deleteWorkout(id) {
+	try {
+		const res = await axios.delete(`${import.meta.env.VITE_BACKEND_URL}/${id}`);
+	} catch (error) {
+		return error;
+	}
 }
